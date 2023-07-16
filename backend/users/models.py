96755fe2204fe54b0efd204655b40affa92397f4
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-07-17 00:41:35 +0900

    created api, users, recipes

---
+++ backend/users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    first_name = models.CharField('Имя', max_length=150, blank=False)
+    last_name = models.CharField('Фамилия', max_length=150, blank=False)
+    email = models.EmailField('Email', max_length=200, unique=True,)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['id',]
+
+    def __str__(self):
+        return self.username
